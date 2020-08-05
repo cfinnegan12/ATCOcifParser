e4e6f19ffbec9fbb9a8a5b1863982e7e16597084
Ciaran Finnegan <cfinnegan12@qub.ac.uk>
2020-08-05 13:59:58 +0100

    Stop Record Tests

--- ATCOcifParserTests/JourneyTests.cs
+++ ATCOcifParserTests/JourneyTests.cs
@@ -26,5 +26,34 @@ namespace ATCOcifParserTests
                 Assert.AreEqual(header.operationDays[i], expected[i]);
             }
         }
+
+        [TestMethod]
+        public void originTest()
+        {
+            string originStr = "QO7000000153640720UQST1";
+            OriginRecord origin = new OriginRecord(originStr.ToCharArray());
+            Assert.AreEqual(origin.time, "0720");
+            Assert.AreEqual(origin.location, "700000015364");
+        }
+
+        [TestMethod]
+        public void destinationTest()
+        {
+            string destStr = "QT7000000015830753   T1  ";
+            DestinationRecord dest = new DestinationRecord(destStr.ToCharArray());
+            Assert.AreEqual(dest.time, "0753");
+            Assert.AreEqual(dest.location, "700000001583");
+        }
+
+        [TestMethod]
+        public void intermediateTest()
+        {
+            string intStr = "QI70000000120507400740B   T1  ";
+            IntermediateRecord intermediate = new IntermediateRecord(intStr.ToCharArray());
+            Assert.AreEqual(intermediate.time, "0740");
+            Assert.AreEqual(intermediate.location, "700000001205");
+            Assert.AreEqual(intermediate.activity, 'B');
+        }
+
     }
 }
